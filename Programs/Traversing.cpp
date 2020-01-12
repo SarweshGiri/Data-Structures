bca892f5d5eb101509ff48c3a4009e7efdc5f040
Sarwesh Giri <54643970+SarweshGiri@users.noreply.github.com>
2020-01-12 12:48:09 +0530

    Add files via upload

---
+++ Programs/Traversing.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[10],i,n;
+	cout<<"Enter the no of elements: ";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	cin>>a[i];
+	cout<<"Traversing of the array is: \n";
+	for(i=1;i<=n;i++)
+	cout<<a[i]<<endl;
+	return 0;
+}
